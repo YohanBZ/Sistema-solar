587ad92bd7d37070e83ff5cb63ab7cc7788ac5ca
YohanBZ <barbozayohan@gmail.com>
2022-06-27 18:40:30 -0300

    accordion-list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
 
 if(tabMenu.length && tabContent.length) {
   tabContent[0].classList.add('ativo');
@@ -17,3 +19,25 @@ if(tabMenu.length && tabContent.length) {
     });
   })
 }
+}
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if(accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+  
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+  
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    })
+  }
+
+}
+initAccordion();
