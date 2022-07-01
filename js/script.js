e11fde7e81ada144c055f31c261ae84b755518bf
YohanBZ <barbozayohan@gmail.com>
2022-07-01 16:20:54 -0300

    menu-mobile

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js'
 
 
 
@@ -13,6 +14,7 @@ initAccordion();
 initTabNav();
 initModal();
 initDropdownMenu();
+initMenuMobile();
 
 
 
